c9005687f742e6cb6746408b97be2319e683bd92
Ah441 <95483420+Ah441@users.noreply.github.com>
2023-10-23 18:23:15 -0400

    Update CollectionFilter.js

--- models/CollectionFilter.js
+++ models/CollectionFilter.js
@@ -5,58 +5,72 @@ export default class CollectionFilter {
         this.model = model;
     }
 
-    applyFilters() {
-      
-        for (let param in this.params) {
-            if (param.toLowerCase() !== 'sort' && param.toLowerCase() !== 'limit' && param.toLowerCase() !== 'offset' && param.toLowerCase() !== 'fields') {
-                let value = this.params[param];
-                this.objectsList = this.objectsList.filter(obj => this.valueMatch(obj[param], value));
-            }
-        }
-        
-        for (let param in this.params) {
+   applyFilters() {
+    for (let param in this.params) {
+        if (param.toLowerCase() !== 'sort' && param.toLowerCase() !== 'limit' && param.toLowerCase() !== 'offset' && param.toLowerCase() !== 'fields') {
             let value = this.params[param];
-            switch (param.toLowerCase()) {
-                case 'sort':
-                    let [sortField, sortOrder] = value.split(',');
-                    this.objectsList.sort((a, b) => {
-                        if (sortOrder && sortOrder.toLowerCase() === 'desc') {
-                            return this.innerCompare(b[sortField], a[sortField]);
-                        }
-                        return this.innerCompare(a[sortField], b[sortField]);
-                    });
-                    break;
-
-                    case 'limit':
-                        let offset = parseInt(this.params.offset || 0);
-                        this.objectsList.sort((a, b) => a.Id - b.Id);
-                        this.objectsList = this.objectsList.slice(offset * parseInt(value), offset * parseInt(value) + parseInt(value ));
-                        break;
-                    
-
-                        case 'fields':
-                            let fieldsToShow = value.split(',');
-                            this.objectsList = this.objectsList.map(obj => {
-                                let newObj = {};
-                                fieldsToShow.forEach(field => {
-                                    newObj[field] = obj[field];
-                                });
-                                return newObj;
-                            });
-                        
-                           
-                            break;
-                        
+            if (!this.objectsList.some(obj => obj.hasOwnProperty(param))) {
+                this.objectsList = [{ error: `Unexistent field '${param}'` }];
+                return;
             }
-            this.objectsList = this.objectsList.filter((obj, index, self) => 
-        index === self.findIndex((t) => this.equal(t, obj)));
+            this.objectsList = this.objectsList.filter(obj => this.valueMatch(obj[param], value));
         }
     }
+
+    for (let param in this.params) {
+        let value = this.params[param];
+        switch (param.toLowerCase()) {
+            case 'sort':
+                let [sortField, sortOrder] = value.split(',');
+                if (!this.objectsList.some(obj => obj.hasOwnProperty(sortField))) {
+                    this.objectsList = [{ error: "Unexistent field" }];
+                    return;
+                }
+
+                this.objectsList.sort((a, b) => {
+                    if (sortOrder && sortOrder.toLowerCase() === 'desc') {
+                        return this.innerCompare(b[sortField], a[sortField]);
+                    }
+                    return this.innerCompare(a[sortField], b[sortField]);
+                });
+                break;
+
+            case 'limit':
+                let offset = parseInt(this.params.offset || 0);
+                this.objectsList.sort((a, b) => a.Id - b.Id);
+                this.objectsList = this.objectsList.slice(offset * parseInt(value), offset * parseInt(value) + parseInt(value));
+                break;
+
+            case 'fields':
+                let fieldsToShow = value.split(',');
+                for(let field of fieldsToShow) {
+                    if (!this.objectsList.some(obj => obj.hasOwnProperty(field))) {
+                        this.objectsList = [{ error: `Unexistent field '${field}'` }];
+                        return;
+                    }
+                }
+
+                this.objectsList = this.objectsList.map(obj => {
+                    let newObj = {};
+                    fieldsToShow.forEach(field => {
+                        newObj[field] = obj[field];
+                    });
+                    return newObj;
+                });
+                break;
+        }
+        this.objectsList = this.objectsList.filter((obj, index, self) => 
+            index === self.findIndex((t) => this.equal(t, obj))
+        );
+    }
+}
+
 
     get() {
         this.applyFilters();
         return this.objectsList;
     }
+
 
     valueMatch(value, searchValue) {
         try {
